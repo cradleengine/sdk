c9300be42b976b34ae1117bf06f125478ab29407
Lors <lors@Lorss-MacBook-Pro-2.local>
2023-01-11 20:14:31 -0500

    testing emitter

--- src/lib/CradleEngine.ts
+++ src/lib/CradleEngine.ts
@@ -1,22 +1,18 @@
 import { EventEmitter } from 'events';
 
-export default class CradleEngine {
-  _eventEmitter: EventEmitter;
-
-  constructor() {
-    this._eventEmitter = new EventEmitter();
-  }
+export const CradleEngine = () => {
+  const _eventEmitter = new EventEmitter();
 
   /**
    * @returns true
    * @anotherNote opens cradle app
    */
-  connectCradleWallet = () => {
+  const connectCradleWallet = () => {
     if (window) {
       window.open(
         `cradlewallet://connectWallet/example/1?hostname=${window.location.hostname}`
       );
-      this._eventEmitter.emit('accountsChanged', [
+      _eventEmitter.emit('accountsChanged', [
         '0x677e813fee748f9467de2f00a5ad9d1d8cf365bb',
       ]);
     } else {
@@ -24,4 +20,8 @@ export default class CradleEngine {
     }
     return true;
   };
-}
+
+  return {
+    connectCradleWallet,
+  };
+};
