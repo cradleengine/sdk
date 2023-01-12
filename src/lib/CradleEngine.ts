552b54bb05a1ad02accebdb8409c84d49aa30176
Lors <lors@Lorss-MacBook-Pro-2.local>
2023-01-11 20:49:09 -0500

    ethereum emitter

--- src/lib/CradleEngine.ts
+++ src/lib/CradleEngine.ts
@@ -1,19 +1,14 @@
-declare global {
-  interface Window {
-    ethereum?: any;
-  }
-}
 export const CradleEngine = () => {
   /**
    * @returns true
    * @anotherNote opens cradle app
    */
-  const connectCradleWallet = () => {
+  const connectCradleWallet = (provider) => {
     if (window) {
       window.open(
         `cradlewallet://connectWallet/example/1?hostname=${window.location.hostname}`
       );
-      window.ethereum.emit('accountsChanged', [
+      provider.emit('accountsChanged', [
         '0x677e813fee748f9467de2f00a5ad9d1d8cf365bb',
       ]);
     } else {
