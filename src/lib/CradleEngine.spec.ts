9647ac2dd01e58305cf9de2ca12e952e634b1e3e
Lors <lors@Lorss-MacBook-Pro-2.local>
2023-01-16 17:34:28 -0500

    removed test

--- src/lib/CradleEngine.spec.ts
+++ src/lib/CradleEngine.spec.ts
@@ -1,9 +1,5 @@
 import test from 'ava';
-import { CradleEngine } from './CradleEngine';
-import CradleProvider from './provider/provider';
 
-test('CradleEngineHasProvider', (t) => {
-  const engine = CradleEngine();
-  console.log(engine.provider.getProviderState());
-  t.is(typeof engine.provider, typeof CradleProvider);
+test('dummyTest', (t) => {
+  t.is(true, true);
 });
