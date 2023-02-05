0e27a8a8dbea457039b4bf548c550c40ccc769de
Lors <lors.kushtov@gmail.com>
2023-02-04 21:07:10 -0500

    cleaner

--- src/lib/CradleEngine.ts
+++ src/lib/CradleEngine.ts
@@ -1,13 +1,10 @@
-import { initializeProvider } from './provider/initialize';
-
 export const CradleEngine = () => {
   /**
    * @returns true
    * @anotherNote Initializes CradleEngine
    */
   const initialize = () => {
-    const _provider = initializeProvider();
-    return _provider;
+    return true;
   };
 
   return {
