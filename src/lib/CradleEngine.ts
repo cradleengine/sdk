98b153da2ce2803200449661f716f58e330171cf
Lors <lors.kushtov@gmail.com>
2023-02-04 21:04:23 -0500

    Clean version of sdk

--- src/lib/CradleEngine.ts
+++ src/lib/CradleEngine.ts
@@ -1,29 +1,16 @@
-import { debug } from './helpers/logger';
 import { initializeProvider } from './provider/initialize';
 
 export const CradleEngine = () => {
-  const _provider = initializeProvider();
   /**
    * @returns true
-   * @anotherNote opens cradle app
+   * @anotherNote Initializes CradleEngine
    */
-  const connectCradleWallet = () => {
-    debug.log('@PROVIDER', _provider);
-    if (window) {
-      window.open(
-        `cradlewallet://connectWallet/example/1?hostname=${window.location.hostname}`
-      );
-      _provider.emit('accountsChanged', [
-        '0x677e813fee748f9467de2f00a5ad9d1d8cf365bb',
-      ]);
-    } else {
-      console.log('window does not exist');
-    }
-    return true;
+  const initialize = () => {
+    const _provider = initializeProvider();
+    return _provider;
   };
 
   return {
-    connectCradleWallet,
-    provider: _provider,
+    initialize,
   };
 };
